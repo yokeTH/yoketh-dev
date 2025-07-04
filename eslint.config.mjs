7aa1c682a449b5e604f487c15447c06d0f271723
Thanapon Johdee <66236295+yokeTH@users.noreply.github.com>
2025-07-04 12:56:13 +0700

    Add Prettier integration to ESLint and pre-commit hook

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,5 @@
 import { FlatCompat } from '@eslint/eslintrc'
+import eslintConfigPrettier from 'eslint-config-prettier/flat'
 import { dirname } from 'path'
 import { fileURLToPath } from 'url'
 
@@ -10,7 +11,12 @@ const compat = new FlatCompat({
 })
 
 const eslintConfig = [
-  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  ...compat.extends(
+    'next/core-web-vitals',
+    'next/typescript',
+    'plugin:prettier/recommended',
+  ),
+  eslintConfigPrettier,
 ]
 
 export default eslintConfig
